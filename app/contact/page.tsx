58bdb9d2af6a7907e6fbd0a0fcca521f83f78cb4
Sudo-Self <sudo-self@proton.me>
2023-11-15 15:28:09 -0700

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/ilostmyipad",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@iLostmyipad",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:Jesse@JesseJesse.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "Jesse@JesseJesse.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/sudo-self",
 		label: "Github",
-		handle: "chronark",
+		handle: "sudo-self",
 	},
 ];
 
