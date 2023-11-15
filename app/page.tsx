c003213f02f49f5969b280fca54c62c6b753bfe0
Sudo-Self <sudo-self@proton.me>
2023-11-15 15:00:31 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,20 +29,20 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        JesseJesse
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
+          Exploring AI with code{" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://ai.jessejesse.com"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            JesseJesse.com
+          </Link>and chat GPT. An AI chat model based on machine learning
         </h2>
       </div>
     </div>
