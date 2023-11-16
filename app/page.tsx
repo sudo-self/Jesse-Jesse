818c03ed86d08e784ebcdbe6919be793e15c4572
Sudo-Self <sudo-self@proton.me>
2023-11-16 02:18:47 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,14 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Exploring A.I. with&nbsp;{" "}
+          Exploring AI thru{" "}
           <Link
             target="_blank"
             href="https://code.jessejesse.com"
-            className="underline duration-500 hover:text-zinc-300"
+            className="underline duration-500 hover:text-lime-400"
           >
             code
-          </Link>&nbsp;and GPT chat models based on machine learning.
+          </Link> and chat GPT models based on machine learning.
         </h2>
       </div>
     </div>
