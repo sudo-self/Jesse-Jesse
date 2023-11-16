d2a0fed077ae517ebaa6358a19583f7bf15303a7
Sudo-Self <sudo-self@proton.me>
2023-11-16 02:48:56 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,21 +28,21 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        JesseJesse
+      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text text-lime-400 ">
+        JesseJesse.com
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          Exploring AI thru{" "}
+          AI Translate{" "}
           <Link
             target="_blank"
             href="https://code.jessejesse.com"
             className="underline duration-500 hover:text-lime-400"
           >
-            code
-          </Link> and chat GPT models based on machine learning.
+           CODE
+          </Link>&nbsp;to any language. 
         </h2>
       </div>
     </div>
